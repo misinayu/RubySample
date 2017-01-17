dffed69388f0d0776c416c1350008030dfe68102
misinayu <yama_masa22@yahoo.co.jp>
2017-01-17 11:58:06 +0900

    test-webserver0ファイルの修正

--- CrawlerDev/test-webserver0.rb
+++ CrawlerDev/test-webserver0.rb
@@ -1,4 +1,4 @@
-#!/usr/local/bun/ruby
+#!/uer/local/bin/ruby
 # -*- coding: utf-8 -*-
 require 'webrick'
 
@@ -8,24 +8,25 @@ class TestContentServlet <
   def do_GET(req, res)
     # 拡張子で分岐
     res.body = case req.path
-    when /\.html$/; html_content req.path
-    when /\.txt$/; txt_content req.path
-    else; "dummy"
+      when /\.html$/; html_content req.path
+      when /\.txt$/; txt_content req.path
+      else; "dummy"
     end
+
     res.content_type = WEBrick::HTTPUtils.mime_type(
       req.path_info, WEBrick::HTTPUtils::DefaultMimeTypes)
   end
 
   def html_content(path)
-    node = path[0..-6]
-<html><head><title>#{paht}</title></head>
+    node = path[0..6]
+    <<HTML
+<html><head><title>#{path}/1.html</a><br>
 <body><p>
 <a href="#{node}/1.html">#{node}/1.html</a><br>
 <a href="#{node}/2.html">#{node}/2.html</a><br>
-<a href="#{node}txt">#{node}.txt</a><br>
+<a href="#{node}.txt">#{node}.txt</a><br>
 <a href="http://localhost:7777#{node}.org">
 #{node}.org</a><br>
-<a href="/1.html">/1.html</a>
 </p></body></html>
 HTML
   end
